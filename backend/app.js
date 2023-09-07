a60dde1c7325de4f2b3ff15eb14b6a27786ecbb6
Blue-Grizzly <123905990+Blue-Grizzly@users.noreply.github.com>
2023-09-07 18:01:58 +0200

    scalability

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
 import express from "express";
 import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
@@ -45,31 +46,30 @@ async function getArtist(request, response) {
     }
 }
 
-async function deleteArtist(request, response) {
+async function postArtist(request, response) {
     const artists = await readArtistDB();
-    const param = request.params.artistid;
-    const newArtists = removeArtist(artists, param);
-    if (newArtists == artists) {
-        response.status(404).json({ error: `No artist found with id: ${param}!` });
+
+    if (!request.body) {
+        response.status(400).json({ error: "No artist recieved!" });
+
+    } else if ((Object.keys(request.body).length != 8)) {
+        // ^finds number of keys/attributes in object and tests if they match the correct amount (9)
+        response.status(400).json({ error: "Incorrect number of artist attributes!" });
+
+    } else if (artists.find(artist => artist.name.toLowerCase() == request.body.name.toLowerCase())) {
+        response.status(400).json({ error: "Artist already exists!" });
+
     } else {
-        writeArtistDB(newArtists);
-        response.json(newArtists);
+        pushNewArtist(request, artists);
+        response.json(artists);
     }
 }
 
-async function favoriteArtist(request, response) {
-    const artists = await readArtistDB();
-    const param = request.params.artistid;
-    const result = findArtist(artists, param);
-
-    if (!result) {
-        response.status(404).json({ error: `No artist found with id: ${param}!` });
-    } else {
-        result.favorite = !result.favorite;
-
-        writeArtistDB(artists);
-        response.json(result.favorite);
-    }
+function pushNewArtist(request, artists) {
+    request.body.id = getNewId(artists);
+    request.body.favorite = false;
+    artists.push(request.body);
+    writeArtistDB(artists);
 }
 
 async function updateArtist(request, response) {
@@ -90,30 +90,33 @@ async function updateArtist(request, response) {
     }
 }
 
-async function postArtist(request, response) {
+async function deleteArtist(request, response) {
     const artists = await readArtistDB();
-
-    if (!request.body) {
-        response.status(400).json({ error: "No artist recieved!" });
-
-    } else if ((Object.keys(request.body).length != 8)) {
-        // ^finds number of keys/attributes in object and tests if they match the correct amount (9)
-        response.status(400).json({ error: "Incorrect number of artist attributes!" });
-
-    } else if (artists.find(artist => artist.name.toLowerCase() == request.body.name.toLowerCase())) {
-        response.status(400).json({ error: "Artist already exists!" });
-
+    const param = request.params.artistid;
+    const newArtists = removeArtist(artists, param);
+    if (newArtists == artists) {
+        response.status(404).json({ error: `No artist found with id: ${param}!` });
     } else {
-        request.body.id = new Date().getTime();
-        request.body.favorite = false; 
-
-        artists.push(request.body);
-        
-        writeArtistDB(artists)
-        response.json(artists);
+        writeArtistDB(newArtists);
+        response.json(newArtists);
     }
 }
 
+async function favoriteArtist(request, response) {
+    const artists = await readArtistDB();
+    const param = request.params.artistid;
+    const result = findArtist(artists, param);
+
+    if (!result) {
+        response.status(404).json({ error: `No artist found with id: ${param}!` });
+    } else {
+       
+        result.favorite = !result.favorite;
+
+        writeArtistDB(artists);
+        response.json(result.favorite);
+    }
+}
 
 async function readArtistDB() {
     return JSON.parse(await fs.readFile("data.json"));
@@ -129,6 +132,11 @@ function writeArtistDB(artists){
 
 function removeArtist(artists, param){
    return artists.filter(artist => Number(artist.id) !== Number(param));
+}
+
+function getNewId(artists){
+    artists.sort((a, b) => b.id - a.id)
+    return artists[0].id+1;
 }
 
 app.listen( port, () =>{
